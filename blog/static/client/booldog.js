e89ee679cb63e6edccf904a228647c983b47a919
silverania <spaceissimo@gmail.com>
2023-05-19 20:33:35 +0200

    deletedstorage,varpassatcumessage

--- blog/static/client/booldog.js
+++ blog/static/client/booldog.js
@@ -3,17 +3,23 @@ let f = function () {
     var height = "";
     var width = "";
     window.addEventListener("message", function (event) {
-        if (!(event.origin.includes('https://localbooldog:9000'))) {
-            console.log("messaggio ignorato");
+        if (!(event.origin.includes('https://localbooldog.com:8000'))) {
+            console.log("messaggio ignorato" + event.origin);
             ("mancata autorizzazione!");
         }
         else {
+            event.source.postMessage({
+                user: user,
+                password: password,
+            },
+                "*"
+            );
             height = event.data.height;
             width = event.data.base;
             iframe.setAttribute('height', height);
             iframe.setAttribute('width', width);
-            if (event.data.reload === true) {
-                document.getElementById('booldogFrame').src = "https://localbooldog:9000/booldog?mainurl=" + this.location.href.toString() + "&user=" + user + "&password=" + password;
+            if (event.data.reload === "true") {
+                document.getElementById('booldogFrame').src = "https://localbooldog.com:8000/booldog?mainurl=" + this.location.href.toString() + "&user=" + user + "&password=" + password;
                 console.log("iframe reload !");
             }
             //iframe.style.height = iframe.contentWindow.document.body.scrollHeight + "px";
@@ -25,12 +31,11 @@ let f = function () {
         let mainurl = location.href.toString();
         iframe.setAttribute("id", "booldogFrame");
         iframe.setAttribute("scrolling", "no");
-        iframe.setAttribute("data-user", user);
-        iframe.setAttribute("data-pd", password);
         iframe.setAttribute("data-auth", "");
         iframe.setAttribute("style", "display:block;margin:100px auto;width:100%;");
-        iframe.setAttribute('src', 'https://localbooldog:9000/booldog?mainurl=' + mainurl + "&user=" + user + "&password=" + password);
+        iframe.setAttribute('src', 'https://localbooldog.com:8000/booldog?mainurl=' + mainurl + "&user=" + user + "&password=" + password);
         const body = document.getElementsByTagName("body")[0];
         body.appendChild(iframe);
     }
+
 }();
